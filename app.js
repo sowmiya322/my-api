524b16113c48d0084e78b539bc1a74893ee87cec
sowmiya322 <sowmiyam002@gmail.com>
2025-02-14 17:07:46 +0530

    Updated product images with valid URLs

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  res.json({ success: true, message: 'Hello, API is working! 🎉' });
+  res.json({ success: true, message: 'Hello, API is working  Succesfully Done By Kumar...,Sowmiya...,Vijay ....! 🎉' });
 });
 
 app.get('/products', (req, res) => {
@@ -23,15 +23,15 @@ app.get('/products', (req, res) => {
       category: 'Fast Food',
       price: 5.99,
       description: 'A delicious cheese burger with fresh lettuce, tomatoes, and melted cheese.',
-      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTQbuKL7NfxTMy1wUymb2tu25NONgb7TU_NE-KOzWF9UfMU5rRz'
+      image: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg'
     },
     {
       id: 2,
       name: 'Pepperoni Pizza',
-      category: 'Fast Food',
+      category: 'Slow Food',
       price: 9.99,
       description: 'A classic pepperoni pizza with a crispy crust and lots of cheese.',
-      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTQbuKL7NfxTMy1wUymb2tu25NONgb7TU_NE-KOzWF9UfMU5rRz'
+      image: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg'
     },
     {
       id: 3,
@@ -39,7 +39,7 @@ app.get('/products', (req, res) => {
       category: 'Italian',
       price: 7.99,
       description: 'Healthy pasta with fresh vegetables, olive oil, and Parmesan cheese.',
-      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTQbuKL7NfxTMy1wUymb2tu25NONgb7TU_NE-KOzWF9UfMU5rRz'
+      image: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg'
     },
     {
       id: 4,
@@ -47,7 +47,7 @@ app.get('/products', (req, res) => {
       category: 'Fast Food',
       price: 6.99,
       description: 'Grilled chicken sandwich with crispy lettuce and creamy mayo.',
-      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTQbuKL7NfxTMy1wUymb2tu25NONgb7TU_NE-KOzWF9UfMU5rRz'
+      image: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg'
     },
     {
       id: 5,
@@ -55,7 +55,7 @@ app.get('/products', (req, res) => {
       category: 'Beverages',
       price: 4.99,
       description: 'Thick and creamy chocolate shake topped with whipped cream.',
-      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTQbuKL7NfxTMy1wUymb2tu25NONgb7TU_NE-KOzWF9UfMU5rRz'
+      image: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg'
     }
   ];
 
